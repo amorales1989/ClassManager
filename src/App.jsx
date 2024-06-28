c382ff14e939df077eb4fd0b3583969f632e1c02
alejandro morales <alejandro.morales@thefuzzyfish.com>
2024-06-27 21:49:59 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = (username, password) => {
+    if (username === 'admin' && password === '123456') { 
+      setIsAuthenticated(true);
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login handleLogin={handleLogin} />} />
+        <Route path="/dashboard" element={
+          isAuthenticated ? <Home /> : <Navigate to="/" />
+        } />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css'; 
+import Button from '@mui/material/Button'
+
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value.toLowerCase()); 
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(username, password);
+    if (username === 'admin' && password === '123456') {
+      navigate('/home');
+    } else {
+      console.log('Credenciales incorrectas');
+    }
+  };
+
+  return (
+    <div className="login-container" style={{ background: 'black' }}>
+      <div className="login-card">
+        <img src="/vite.svg" alt="Logo" className="LogoIndulac3.0" />
+        <h2>Login</h2>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="username">Usuario:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange} 
+            required
+          />
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // No es necesario modificar la contraseña
+            required
+          />
+          <Button type="submit" variant="contained">Iniciar Sesión</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+ 
+export default Login;
